deb309e35fa24f87c11608de88541140ee5ec89e
Johnny Avakian <javak@yahoo.com>
2026-05-09 09:41:05 -0700

    Add copyright attribution to copied text from blog posts

--- script.js
+++ script.js
@@ -752,3 +752,15 @@ const initBlogIndex = () => {
 };
 
 initBlogIndex();
+
+// Copyright attribution for copied text
+document.addEventListener("copy", function (event) {
+  const selection = window.getSelection();
+  if (selection.toString().length > 0) {
+    const attribution = "\n\nWritten by Johnny Avakian. Any republication without author's written permission is against the law.";
+    const clipboardData = event.clipboardData || window.clipboardData;
+    const copiedText = selection.toString() + attribution;
+    clipboardData.setData("text/plain", copiedText);
+    event.preventDefault();
+  }
+});
